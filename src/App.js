846a61035bc753c052c6a5d0764ba1484770a3a4
XWeb-CoderX <rileytheangel75@gmail.com>
2023-05-22 21:03:18 +0100

    add: gradient type

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+export default function App() {
+  const [leftColor, setLeftColor] = useState("#fff700");
+  const [rightColor, setRightColor] = useState("#ff0000");
 
-function App() {
+  const [radialGradient, setRadialGradient] = useState(
+    `radial-gradient(${leftColor} , ${rightColor})`
+  );
+  const [linearGradient, setLinearGradient] = useState(
+    `linear-gradient(to right , ${leftColor} , ${rightColor})`
+  );
+  const [gradientType, setGradientType] = useState("radial");
+
+  const copyCode = () => {
+    navigator.clipboard.writeText(
+      `linear-gradient(to right, ${leftColor} , ${rightColor})`
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        className="container"
+        style={{
+          background:
+            gradientType === "radial" ? radialGradient : linearGradient,
+          height: "90vh",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <div className="left-color">
+          <input
+            value={leftColor}
+            onChange={(event) => {
+              console.log(event.target.value);
+              setLeftColor(event.target.value);
+              setLinearGradient(
+                `linear-gradient(to right, ${leftColor} , ${rightColor})`
+              );
+              setRadialGradient(
+                `raidal-gradient(${leftColor} , ${rightColor})`
+              );
+            }}
+            type="color"
+          />
+          <div className="color" style={{ backgroundColor: "white" }}>
+            <span>{leftColor}</span>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(leftColor);
+              }}
+              class="clip"
+            >
+              📋
+            </button>
+          </div>
+        </div>
+
+        <div className="right-color">
+          <input
+            value={rightColor}
+            onChange={(event) => {
+              setRightColor(event.target.value);
+              setLinearGradient(
+                `linear-gradient(to right, ${leftColor} , ${rightColor})`
+              );
+              setRadialGradient(
+                `raidal-gradient(${leftColor} , ${rightColor})`
+              );
+            }}
+            type="color"
+          />
+
+          <div className="color" style={{ backgroundColor: "white" }}>
+            {" "}
+            <span>{rightColor}</span>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(rightColor);
+              }}
+              class="clip"
+            >
+              📋
+            </button>{" "}
+          </div>
+        </div>
+      </div>
+
+      <div style={{ textAlign: "center" }} className="panel">
+        <button
+          onClick={() => {
+            setGradientType("linear");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Linear Gradient
+        </button>
+        <button onClick={copyCode}> Copy Gradient code </button>
+        <button
+          onClick={() => {
+            setGradientType("radial");
+          }}
+        >
+          Radial Gradient
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
